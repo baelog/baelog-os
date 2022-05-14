66d0a45118e3af48a35645f6196af35eaf6b2df8
brassart <florent.brassart@epitech.eu>
2022-05-15 00:21:41 +0200

    printing 16 bits register hex

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,61 +4,23 @@
 
 [org 0x7c00]
 
-mov bx, hex_prefix
-; add bx, 0x7c00        ; not needed anymore
+    ; mov bx, my_string
+    ; add bx, 0x7c00        ; not needed anymore
+    ; call print_string
 
-call print_string
-; call print_hexa
+    mov dx, 0x1234
+    call print_hex
 
-jmp $
+    jmp $
 
 %include "./print_string.asm"
-
-print_hexa:
-    pusha
-    mov dx, 0xf000
-    mov cx, bx
-    and bx, ax
-    call set_char
-    call print_char
-
-loop_hexa:
-    shr dx, 4
-    mov bx, cx
-    and bx, ax
-
-    call set_char
-    call print_char
-    cmp dx, 0x000f
-    je end_print_hexa
-    jmp loop_hexa
-
-end_print_hexa:
-    popa
-    ret
-
-switch_to_char:
-    cmp bx, 0x0a
-    jl set_char
-    add bx, 0x11
-
-set_char:
-    add bx, 0x30
-    ret
-
+%include "./print_hexa.asm"
 
 my_string:
-    db "Hello World", 0
-
-hex_prefix:
-    db "0x", 0
+    db "Hello World\n", 0
 
 
-times 510-($-$$) db 0       ; When compiled , our program must fit into 512 bytes ,
-                            ; with the last two bytes being the magic number ,
-                            ; so here , tell our assembly compiler to pad out our
-                            ; program with enough zero bytes ( db 0) to bring us to the
-                            ; 510 th byte.
 
-dw 0xaa55                   ; Last two bytes ( one word ) form the magic number ,
-                            ; so BIOS knows we are a boot sector.
+; padding and magic number
+times 510-($-$$) db 0
+dw 0xaa55
